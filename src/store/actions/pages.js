ea346a12f5f40646a09c3eb3bfddce4674b1c56f
Jorj <ggkankava@gmail.com>
2021-05-01 21:13:28 +0400

    rollback

--- src/store/actions/pages.js
+++ src/store/actions/pages.js
@@ -2,7 +2,7 @@ import { apiCall } from "../../services/api";
 
 export const fetchPage = (path, callback) => {
   return new Promise((resolve, reject) => {
-    return apiCall("get", `/api/${path}`)
+    return apiCall("get", `https://testapi.cryptoiex.io/api/${path}`)
       .then((data) => {
         callback(data);
         resolve();
